25df8ec112ecdd3d45041cca249162fb4c701bfa
Hiba Sabouh <hiba.sabouh.98@gmail.com>
2025-02-06 19:33:22 +0300

    fix category accordion expand and collapse

--- resources/views/admin/recursive/checkbox-sub-category.blade.php
+++ resources/views/admin/recursive/checkbox-sub-category.blade.php
@@ -1,9 +1,9 @@
 @forelse ($categories as $subCategory)
 <div class="px-3">
 
-<div class="accordion-item">
+<div class="accordion-item" id="{{$subCategory->name}}-collapse-area">
     <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="{{'#'.$subCategory->name}}" aria-expanded="false" aria-controls="collapseThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="{{'#'.$subCategory->name}}" aria-expanded="false" aria-controls="{{$subCategory->name}}">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" name="selectedCategoriesIDArray[]" value="{{$subCategory->id}}" id="{{$subCategory->name}}" {{in_array($subCategory->id,$selectedCategoriesIDArray)?'checked':''}}>
                 <label class="form-check-label" for="{{$subCategory->name}}">
@@ -13,7 +13,7 @@
 
         </button>
     </h2>
-    <div id="{{$subCategory->name}}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+    <div id="{{$subCategory->name}}" class="accordion-collapse collapse" data-bs-parent="#{{$subCategory->name}}-collapse-area">
         <div class="accordion-body">
             @include('admin.recursive.checkbox-sub-category', ['categories' => $subCategory->children()])
         </div>
--- resources/views/admin/recursive/radio-sub-category.blade.php
+++ resources/views/admin/recursive/radio-sub-category.blade.php
@@ -1,21 +1,20 @@
 @forelse ($categories as $subCategory)
-<div class="px-3"> 
+<div class="px-3">
 
-<div class="accordion-item">
+<div class="accordion-item" id="{{$subCategory->name}}-collapse-area">
     <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="{{'#'.$subCategory->name}}" aria-expanded="false" aria-controls="collapseThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="{{'#'.$subCategory->name}}" aria-expanded="false" aria-controls="{{$subCategory->name}}">
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="parent_id" value="{{$subCategory->id}}" {{$subCategory->id==$category->parent_id?"checked":""}} >
                 <label class="form-check-label" for="{{$subCategory->name}}">
                     {{$subCategory->name}}
                 </label>
             </div>
-                        
         </button>
     </h2>
-    <div id="{{$subCategory->name}}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+    <div id="{{$subCategory->name}}" class="accordion-collapse collapse" data-bs-parent="#{{$subCategory->name}}-collapse-area">
         <div class="accordion-body">
-            @include('admin.recursive.radio-sub-category', ['categories' => $subCategory->children()])        
+            @include('admin.recursive.radio-sub-category', ['categories' => $subCategory->children()])
         </div>
     </div>
 </div>
@@ -23,5 +22,5 @@
 </div>
 
 @empty
-    
+
 @endforelse
